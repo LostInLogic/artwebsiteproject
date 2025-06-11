1ec67ae2487dcc0a9a73c6e3e7673a199b635655
Charles Robinson <charlesdalerobinson@gmail.com>
2025-06-10 19:49:40 -0700

    Added all rights reserved to copyright text.

--- artwebsite/src/Footer/Footer.js
+++ artwebsite/src/Footer/Footer.js
@@ -40,7 +40,7 @@ const Footer = () => {
           </Link>
         </div>
         <div className="copyright-text">
-          <p>&copy; 2025 ART WITH LARISSE</p>
+          <p>&copy; 2025 Art With Larisse - All Rights Reserved</p>
         </div>
       </div>  
       <div className="footer-img">
